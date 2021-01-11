1a303c4dc957f5995ee3a4dd9dc6cdeea5683c4b
Naomi Gallupe <gall0381@algonquinlive.com>
2021-01-11 15:13:40 -0500

    Inital commit

--- helloStudents.js
+++ helloStudents.js
@@ -1,9 +1,20 @@
-'use strict'
 
 // 1. Read the JSON file into a variable called students
+const students = require('./students.json')
 
 // 2. Iterate over the students array and print Hello with their full names to the console
-// e.g. Hello Walter Baker
+students.forEach(function(student){ 
+    console.log(`Hello ${student.firstName} ${student.lastName}`) 
+})
 
 // 3. Print out the number of last names starting with the letter D
-// e.g. Count of last names starting with D is 1
+
+let total = 0;
+students.forEach(function(student){ 
+    let name = new Array(student.lastName).filter(last => student.lastName.charAt(0) == 'D')
+    if (name.length === 1) {
+        total++;
+    }
+})
+
+console.log("Count of all last names starting with D is " + total);
